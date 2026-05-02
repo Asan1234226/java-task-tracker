b225a99e340ca2d498b6bdfc03475428ecfd277c
Asan <asan46904@gmail.com>
2026-05-02 14:07:41 +0500

    добавление задачи файл

--- src/Task.java
+++ src/Task.java
@@ -1,13 +1,22 @@
+import java.io.FileWriter;
+import java.util.ArrayList;
+
 public class Task {
     private int id;
     private String title;
     private String description;
     private Tasktatus status;
 
-    public Task(String newTitle, String newDescription) {
+    public Task(String newTitle, String newDescription) throws Exception {
         status = Tasktatus.NEW;
         title = newTitle;
         description = newDescription;
+        FileWriter writer = new FileWriter("tasks.txt");
+        writer.write("id" + "," + "title" + "," + "description" + "," + "status" + "\n");
+        for (int i = 0; i < 1; i++) {
+            writer.write(i + 1 + "," + getTitle() + "," + getDescription() + "," + getStatus() + "\n");
+        }
+        writer.close();
     }
 
     public int getId() {
